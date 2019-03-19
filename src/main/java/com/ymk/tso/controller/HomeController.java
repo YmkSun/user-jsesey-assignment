aa53c02a09171b1069b45be43da02e8a3e75deee
Ye Myo Kyaw <ymksun90@gmail.com>
2019-03-20 03:20:17 +0630

    Skeleton Structure

---
+++ src/main/java/com/ymk/tso/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.ymk.tso.controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/home")
+public class HomeController {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getTestService() {
+		return "Hello World! This is coming from webservice!!";
+	}
+}
